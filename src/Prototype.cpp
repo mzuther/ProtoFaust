4ef9fb34a5e78b728c11d76344c73257f6483a50
Martin Zuther <code@mzuther.de>
2019-07-21 14:42:44 +0200

    cleaned up white space

--- src/Prototype.cpp
+++ src/Prototype.cpp
@@ -2,29 +2,29 @@
 
 
 struct Prototype : Module {
-	enum ParamIds {
-		PITCH_PARAM,
-		NUM_PARAMS
-	};
-	enum InputIds {
-		PITCH_INPUT,
-		NUM_INPUTS
-	};
-	enum OutputIds {
-		SINE_OUTPUT,
-		NUM_OUTPUTS
-	};
-	enum LightIds {
-		BLINK_LIGHT,
-		NUM_LIGHTS
-	};
+    enum ParamIds {
+        PITCH_PARAM,
+        NUM_PARAMS
+    };
+    enum InputIds {
+        PITCH_INPUT,
+        NUM_INPUTS
+    };
+    enum OutputIds {
+        SINE_OUTPUT,
+        NUM_OUTPUTS
+    };
+    enum LightIds {
+        BLINK_LIGHT,
+        NUM_LIGHTS
+    };
     float phase = 0.f;
     float blinkPhase = 0.f;
 
-	Prototype() {
-		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-		configParam(PITCH_PARAM, 0.f, 1.f, 0.f, "");
-	}
+    Prototype() {
+        config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+        configParam(PITCH_PARAM, 0.f, 1.f, 0.f, "");
+    }
 
     void process(const ProcessArgs &args) override {
         // Compute the frequency from the pitch parameter and input
@@ -36,8 +36,10 @@ struct Prototype : Module {
 
         // Accumulate the phase
         phase += freq * args.sampleTime;
-        if (phase >= 0.5f)
+
+        if (phase >= 0.5f) {
             phase -= 1.f;
+        }
 
         // Compute the sine output
         float sine = std::sin(2.f * M_PI * phase);
@@ -47,31 +49,34 @@ struct Prototype : Module {
 
         // Blink light at 1Hz
         blinkPhase += args.sampleTime;
-        if (blinkPhase >= 1.f)
+
+        if (blinkPhase >= 1.f) {
             blinkPhase -= 1.f;
+        }
+
         lights[BLINK_LIGHT].setBrightness(blinkPhase < 0.5f ? 1.f : 0.f);
     }
 };
 
 
 struct PrototypeWidget : ModuleWidget {
-	PrototypeWidget(Prototype *module) {
-		setModule(module);
-		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/Prototype.svg")));
+    PrototypeWidget(Prototype *module) {
+        setModule(module);
+        setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/Prototype.svg")));
 
-		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+        addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+        addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+        addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+        addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(15.24, 46.063)), module, Prototype::PITCH_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(15.24, 46.063)), module, Prototype::PITCH_PARAM));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 77.478)), module, Prototype::PITCH_INPUT));
+        addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 77.478)), module, Prototype::PITCH_INPUT));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 108.713)), module, Prototype::SINE_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 108.713)), module, Prototype::SINE_OUTPUT));
 
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(15.24, 25.81)), module, Prototype::BLINK_LIGHT));
-	}
+        addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(15.24, 25.81)), module, Prototype::BLINK_LIGHT));
+    }
 };
 
 
--- src/plugin.cpp
+++ src/plugin.cpp
@@ -5,11 +5,11 @@ Plugin *pluginInstance;
 
 
 void init(Plugin *p) {
-	pluginInstance = p;
+    pluginInstance = p;
 
-	// Add modules here
-	p->addModel(modelPrototype);
+    // Add modules here
+    p->addModel(modelPrototype);
 
-	// Any other plugin initialization may go here.
-	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
+    // Any other plugin initialization may go here.
+    // As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
 }
